a8060a17c681fdfecce7633be5c4b735b5c96252
Ivan Porollo <iporollo@gmail.com>
2022-12-11 13:53:29 -0800

    add favicon & remove unnecessary files

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,10 @@ export default function Home({ linkData }) {
           name="description"
           content="A curated list of new developments & discoveries in Artificial Intelligence"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤖</text></svg>"
+        />
       </Head>
 
       <div className={styles.header}>
