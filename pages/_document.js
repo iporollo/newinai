0244cd475093121dc9e1dd0e7cb7411e104547b4
Ivan Porollo <iporollo@gmail.com>
2023-01-03 01:03:17 -0500

    update gtag script

--- pages/_document.js
+++ pages/_document.js
@@ -14,15 +14,23 @@ class MyDocument extends Document {
             rel="icon"
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤖</text></svg>"
           />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TAG}`}
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
                 window.dataLayer = window.dataLayer || [];
-                function gtag(){window.dataLayer.push(arguments);}
+                function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_TAG}');
-              `}
-          </Script>
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_TAG}', {
+                });
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
